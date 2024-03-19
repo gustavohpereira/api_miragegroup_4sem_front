2773708d85c6553be1c9a130e2511b01b1fbfa23
gustavohpereira <gustavohpa2003@gmail.com>
2024-03-19 18:41:38 -0300

    create react app with tailwind

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# API_MirageGroup_4sem_front
+API_MirageGroup_4sem_front
---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return(
+
+  <div className=' w-full h-screen flex items-center justify-center text-6xl'>
+    teste
+  </div>
+
+  )
+  
+  
+}
+
+export default App;
