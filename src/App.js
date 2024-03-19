d0540ec675785c6cd80fbef6de9c02ec73961a8e
gustavohpereira <gustavohpa2003@gmail.com>
2024-03-19 20:54:57 -0300

    add new meeting component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import NewMeeting from "./components/newMeeting";
 import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <div className="flex flex-col lg:flex-row">
       <Sidebar></Sidebar>
-      
+      <NewMeeting></NewMeeting>
      
     </div>
   );
