5d37ea31e4f4fc4f1ee58184f16edddb5f4bea8f
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2024-06-03 11:09:51 -0300

    fix width of general content

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -4,7 +4,7 @@ export default function PageWrapper({ children }) {
     return (
         <div className='flex_start lg:flex-row w-full flex-col'>
             <Sidebar></Sidebar>
-            <div className='w-full lg:w-[90%] p-4 lg:p-8 '>
+            <div className='w-full lg:w-[85%] p-4 lg:p-4 '>
                 {children}
             </div>
         </div>
