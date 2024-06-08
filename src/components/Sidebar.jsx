e546d1c8c27460975f4f8f1618b41368198419ac
Gisele Reis <giselealvesdosreis@gmail.com>
2024-06-08 00:07:47 -0300

    :lipstick: style: botao logout e subrota updateMeeting

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,7 +33,7 @@ export default function Sidebar() {
       name: "Reuniões",
       icon: "/meeting.svg",
       link: "/meetings",
-      subRoutes: ["/createMeeting"]
+      subRoutes: ["/createMeeting", "/updateMeeting/:meetingId"]
     },
     {
       name: "Usuários",
@@ -43,7 +43,22 @@ export default function Sidebar() {
     },
   ];
 
- return (
+  const isActiveLink = (link) => {
+    if (location.pathname === link.link) {
+      return true;
+    }
+
+    if (link.subRoutes) {
+      return link.subRoutes.some((subRoute) => {
+        const regex = new RegExp(`^${subRoute.replace(/:\w+/g, '\\w+')}$`);
+        return regex.test(location.pathname);
+      });
+    }
+
+    return false;
+  };
+
+  return (
     <div className="h-16 w-full lg:h-screen lg:min-w-[15rem] lg:max-w-[15rem] sticky top-0 bg-[#262932] lg:w-sidebar-width border-r border-sidebar-border flex lg:flex-col justify-between items-center lg:py-6">
       <div className="lg:w-full w-[80%] flex lg:flex-col justify-between items-center lg:items-start lg:py-6">
         <div className="hidden lg:flex justify-center items-center py-6 w-full">
@@ -51,7 +66,7 @@ export default function Sidebar() {
         </div>
         <div className="flex lg:flex-col justify-center lg:justify-start items-center px-2 lg:px-6 lg:gap-8 w-full">
           {links.map((link) => {
-            const isActive = location.pathname === link.link || (link.subRoutes && link.subRoutes.includes(location.pathname));
+            const isActive = isActiveLink(link);
 
             if (link.name === 'Usuários' && user.role === 'admin') {
               return (
@@ -92,11 +107,11 @@ export default function Sidebar() {
         </div>
       </div>
 
-      <div className="lg:w-full w-[20%] flex justify-center lg:justify-start">
-        <button type="button" className="flex lg:flex-col text-[#FEFEFE] lg:w-42 p-2 w-full gap-4 hover:bg-[#575757] rounded-lg justify-center lg:justify-center lg:items-center" onClick={handleLogoutSidebar}>
-          <div className="lg:w-1/6 w-[35%]  flex justify-center lg:justify-start">
+      <div className="lg:w-full w-[20%] flex justify-center">
+        <button type="button" className="flex text-[#FEFEFE] lg:w-42 gap-4 p-4 hover:bg-[#575757] rounded-lg justify-center lg:justify-center lg:items-center" onClick={handleLogoutSidebar}>
+          <div className="w-6 flex justify-center lg:justify-start">
             <img src="/logout.svg" alt="Logout" />
-          </div>
+          </div>  
           <label className="cursor-pointer hidden lg:block">Sair da conta</label>
         </button>
       </div>
